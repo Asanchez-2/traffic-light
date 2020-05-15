8327ed4f1f2b73a7a20ce808ab24ea378368006e
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-05-15 15:44:32 +0000

    Se añaden cambios al ejercicio

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -6,14 +6,38 @@ import ReactDOM from "react-dom";
 import "bootstrap";
 
 export class TrafficLight extends React.Component {
+	state = {
+		activate: false
+	};
 	render() {
 		return (
 			<div className="main">
 				<div className="stick" />
 				<div className="trafficBody rounded">
-					<div className="red rounded-circle" />
-					<div className="orange rounded-circle" />
-					<div className="green rounded-circle" />
+					<div className="red">
+						<div
+							className={this.state.active ? "glow" : "red"}
+							onClick={() =>
+								this.setState({ active: !this.state.active })
+							}
+						/>
+					</div>
+					<div className="orange">
+						<div
+							className={this.state.active ? "glow" : "orange"}
+							onClick={() =>
+								this.setState({ active: !this.state.active })
+							}
+						/>
+					</div>
+					<div className="green">
+						<div
+							className={this.state.active ? "glow" : "green"}
+							onClick={() =>
+								this.setState({ active: !this.state.active })
+							}
+						/>
+					</div>
 				</div>
 			</div>
 		);
